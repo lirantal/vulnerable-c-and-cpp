30756da70f035b270b15a0f2f3503919dd4d075a
Liran Tal <liran.tal@gmail.com>
2024-06-17 11:22:09 +0300

    docs: provide more context for the program
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- program1.c
+++ program1.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 
 void allocateMemory() {
+    // Use-case:
+    // For example, let's say you're writing a function to read a file into memory.
+    // You don't know the size of the file until runtime, so you can't use a fixed-size array.
+    // Instead, you can use malloc to allocate exactly the right amount of memory once you know the file size.
     int *ptr = (int *)malloc(10 * sizeof(int));
     if (ptr == NULL) {
         printf("Memory allocation failed\n");
         return;
     }
     // Do something with ptr
-
     // ❌ Forget to free the memory
 }
 
@@ -26,6 +29,9 @@ int main() {
 void allocateMemory() {
     int arr[10]; // Use static allocation
     // Do something with arr
+
+    // OR, if you manually allocate the memory with malloc(), free it after use:
+    // free(ptr);
 }
 
 int main() {
